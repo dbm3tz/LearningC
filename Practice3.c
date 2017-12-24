f1c12e4f69950bf66f3f2c0694934dcce4dac5ef
bm <dbm3tz@virginia.edu>
2017-12-24 14:10:07 -0500

    Completed tutorial 3 and updated file

--- Practice3.c
+++ Practice3.c
@@ -2,3 +2,34 @@
   https://www.cprogramming.com/tutorial/c/lesson3.html
   Lesson 3: Loops
 */
+
+#include <stdio.h>
+
+int main()
+{
+
+  int x;
+  for (x = 0; x < 4; x++) {
+    if (x == 2) {
+      break;	
+    }
+    printf("x: %d\n", x);
+  }
+
+  int y = 0;
+  while (y < 4) {
+    if (y == 2) {
+      y++;
+      continue;
+    }
+    printf("y: %d\n", y);
+    y++;
+  }
+
+  int z = 0;
+  do {
+    printf("do-while executed once");
+  } while(z != 0);
+
+  getchar();
+}
